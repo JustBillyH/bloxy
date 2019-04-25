e9021e2ecc6a3c42bb74bc64da50e1bdaed9e000
JustBillyH <43467728+JustBillyH@users.noreply.github.com>
2019-04-25 10:05:31 +0100

    Update setRank.js

--- src/group/methods/setRank.js
+++ src/group/methods/setRank.js
@@ -1,25 +1,23 @@
 
 
-exports.run = async function (userIdentifier, roleIdentifier, groupIdentifier) {
-    
-	let [validUser, userId] = this.self.misc.identifier.isUser(userIdentifier);
-	let [validRole, roleId] = this.self.misc.identifier.isRole(roleIdentifier);
-	let [validGroup, groupId] = this.self.misc.identifier.isGroup(groupIdentifier || this.groupId);
-    
-	if (!validUser || !validRole || !validGroup) throw new Error("Must provide a valid user, role and group");
+module.exports = async function (setup, self) {
+    let newPromise = new Promise(async function(resolve, reject) {
+        if (!setup || !self) return reject(`Did not provide setup or self`)
 
-	let url = `https://www.roblox.com/groups/api/change-member-rank?groupId=${groupId}&newRoleSetId=${roleId}&targetUserId=${userId}`;
 
-	let options = {
-		method: "POST"
-	};
+        let url = `https://www.roblox.com/groups/api/change-member-rank?groupId=${setup.groupId}&newRoleSetId=${setup.roleId}&targetUserId=${setup.userId}`;
+        
+        let options = {
+            method: "POST"
+        }
 
-	let response = await this.self._setup.request.request(url, options);
-	if (response.statusCode !== 200) throw new Error(`Failed to set rank. ${response.status}`);
-
-	return true;
-};
-
+        self._request(url, options).then(({res})=>{
+            if (res.statusCode !== 200) return reject(`Failed to set rank, code: ${res.statusCode}, message: ${res.statusMessage}`);
+            resolve(true);
+        }).catch(reject);
+    })
+    return newPromise;
+}
 
 exports.conf = {
 	required: {
